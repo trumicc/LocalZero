a6c8d5af43b99faed29ce70e410f489d38e9fb0f
trumicc <163108637+trumicc@users.noreply.github.com>
2026-04-22 14:21:05 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/localzero/localzero/LocalzeroApplication.java
@@ -0,0 +1,13 @@
+package com.localzero.localzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalzeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localzero/localzero/LocalzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localzero.localzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalzeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
