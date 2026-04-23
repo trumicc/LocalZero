d3a4594044817ae953440064406bf656b0949d92
trumicc <163108637+trumicc@users.noreply.github.com>
2026-04-23 08:49:11 +0200

    User
    User definerat enlgit krav

--- src/main/java/com/localzero/localzero/model/User.java
+++ src/main/java/com/localzero/localzero/model/User.java
@@ -1,4 +1,54 @@
 package com.localzero.localzero.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String passwordHash;
+    @Column (nullable = false)
+    private String location;
+    @Column(nullable = false)
+    private  String roles;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    public User(){}
+
+    public User(String name, String email, String passwordHash, String location, String roles, LocalDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.location = location;
+        this.roles = roles;
+        this.createdAt = LocalDateTime.now();
+    }
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPasswordHash() {return passwordHash;}
+    public void setPasswordHash(String passwordHash) {this.passwordHash = passwordHash;}
+    public String getLocation() {return location;}
+    public void setLocation(String location) {this.location = location;}
+    public String getRoles() {return roles;}
+    public void setRoles(String roles) {this.roles = roles;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
+
+
 }
