51f775ac8d1f6a1a53c3df67543b389ad969584e
trumicc <163108637+trumicc@users.noreply.github.com>
2026-04-26 14:00:03 +0200

    Sec.Config och User Service

--- src/main/java/com/localzero/localzero/security/SecurityConfig.java
+++ src/main/java/com/localzero/localzero/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.localzero.localzero.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
+
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers ("/api/auth/register", "/api/auth/login", "/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                        .sessionManagement(session -> session
+                                .maximumSessions(10)
+                        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/localzero/localzero/service/UserService.java
+++ src/main/java/com/localzero/localzero/service/UserService.java
@@ -1,4 +1,50 @@
 package com.localzero.localzero.service;
 
+import com.localzero.localzero.model.User;
+import com.localzero.localzero.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public User register(User user) {
+        if(userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already registered");
+        }
+
+            String passwordHash = passwordEncoder.encode(user.getPasswordHash());
+            user.setPasswordHash(passwordHash);
+
+            if ("community_organizer".equals(user.getRoles())) {
+                user.setRoles("community_organizer");
+            } else {
+                user.setRoles("resident");
+            }
+            return userRepository.save(user);
+
+        }
+
+        public Optional<User> findByEmail(String email){
+            return userRepository.findByEmail(email.toLowerCase());
+        }
+
+        public boolean checkPassword(String rawPassword, String hashedPassword) {
+            return passwordEncoder.matches(rawPassword, hashedPassword);
+        }
+
+        public Optional<User> findById(Long id) {
+            return userRepository.findById(id);
+        }
+
 }
